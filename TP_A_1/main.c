b742ef94ae53d1f4f7aa2ae55a888146ef9e61fd
PaulaViriato <paulajeniffer1@gmail.com>
2017-11-28 21:15:28 -0200

    Add files via upload

--- TP_A_1/main.c
+++ TP_A_1/main.c
@@ -6,38 +6,35 @@
  *   corretamente a partir da interrupcao de tempo.                           *
  *                                                                            *
  *   -> LED VERMELHO = 0x01                                                   *
- *   -> time = variavel para calcular estado de tempo                         *
- *   -> 1 segundo e CCR0 =  62500 (2 * 62500 * 8 us = 1 segundo)              *
+ *   -> 1 segundo e CCR0 =  3000 (TACTL = TASSEL_1 + MC_1 + ID_2)             *
  ******************************************************************************/
 
 #include "msp430g2553.h"  
-#include <stdio.h>
-#define uint8_t unsigned short
-volatile uint8_t  time;
 
 void main(void) {
  
   WDTCTL = WDTPW + WDTHOLD;   // Desliga Watchdog timer
   P1DIR = 0x01;               // Pino 1.0 como saida (00000001=1[DEC]=1[HEX])
 
-  time = 2;                   // Contador de segundos para acender (2 segundos)
-  CCTL0 = CCIE;               // Habilita interrupcao de comparacao do timer A           
-  TACTL = TASSEL_2+MC_3+ID_3; // SMCLK = 1 MHz, SMCLK/8 = 125 KHz (8 us)      
-  CCR0 =  62500;              /* Modo up/down: chega no valor e depois volta
-                                 para zero, portanto cada interrupcao acontece
-                                 2 * 62500 * 8 us = 1 segundo    */
+  BCSCTL1 = CALBC1_1MHZ;      // Define clock como 1 MHz
+  DCOCTL = CALDCO_1MHZ;       // Define clock do time como 1 MHz
+  
+  BCSCTL3 = BCSCTL3 | LFXT1S_2;                // LFXT1 = VLO
+  IFG1 = IFG1 & (~OFIFG);                      // Limpa a flag OSCFault
+  BCSCTL2 = BCSCTL2 | (SELM_0+DIVM_3+DIVS_3);  // MCLK = DCO/8 , SMCLK = DCO/8
 
+  CCTL0 = CCIE;                // Habilita interrupcao de comparacao do timer A
+  TACTL = TASSEL_1+MC_1 +ID_2; // ACLK = 32 KHz, ACLK/4 = 8 KHz (0.5 us)
+  CCR0 =  3000;                // Modo Up: chega no valor e chama a interrupcao
+                               // 3000 * 0.5 us = 1 segundo
+
+  P1OUT = P1OUT & 0x00;      // Apaga todos os LEDS
   _BIS_SR(CPUOFF + GIE);      // Modo de baixo consumo com interrupcoes
   while(1);                   // Loop infinito
 } 
 
 #pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer_A (void) {   
-    if (time == 0){             // Se faltam 0 segundos para acender...
-        P1OUT = P1OUT ^ 0x01;   // Acende LED vermelho 0x01
-        time = 2;               // Muda o tempo para 2 segundos para acender
-    } else {                    // Se ainda falta tempo para acender...
-        P1OUT = P1OUT & 0xfe;   // LED vermelho permanece desligado
-        time --;                // Diminui o tempo para acender
-    }
+__interrupt void Timer_A (void) {
+    P1OUT = P1OUT ^ 0x01;   // Troca o estado do LED vermelho (0x01)
+    P1IFG = 0x00;           // Zera flag de interrupcao (00000000)
 } 
